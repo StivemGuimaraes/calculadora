ad09b94ba3a322a1f1ef1e8a8502d5bfd78b42a6
stivemguimarães <92881551+steven-hello-world@users.noreply.github.com>
2021-11-17 16:48:37 -0300

    calculadora em c#
    colocando esse repositório no github

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -10,6 +10,63 @@ namespace calculadora
     {
         static void Main(string[] args)
         {
+            numeros p = new numeros();
+            while (p.aux == 1)
+            {
+                Console.WriteLine("digite 1 para adiçao");
+                Console.WriteLine("digite 2 para substração");
+                Console.WriteLine("digite 3 para multiplicação");
+                Console.WriteLine("digite 4 para divisão");
+                p.cal = int.Parse(Console.ReadLine());
+                switch (p.cal)
+                {
+                    case 1:
+                        Console.WriteLine("digite 1 numero");
+                        p.num1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("digite 2 numero");
+                        p.num2 = float.Parse(Console.ReadLine());
+                        p.total = p.num1 + p.num2;
+                        Console.WriteLine("o total é " + p.total);
+                        break;
+                    case 2:
+                        Console.WriteLine("digite 1 numero");
+                        p.num1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("digite 2 numero");
+                        p.num2 = float.Parse(Console.ReadLine());
+                        p.total = p.num1 - p.num2;
+                        Console.WriteLine("o total é " + p.total);
+                        break;
+                    case 3:
+                        Console.WriteLine("digite 1 numero");
+                        p.num1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("digite 2 numero");
+                        p.num2 = float.Parse(Console.ReadLine());
+                        p.total = p.num1 * p.num2;
+                        Console.WriteLine("o total é " + p.total);
+                        break;
+                    case 4:
+                        Console.WriteLine("digite 1 numero");
+                        p.num1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("digite 2 numero");
+                        p.num2 = float.Parse(Console.ReadLine());
+                        if (p.num1 == 0 || p.num2 == 0)
+                        {
+                            Console.WriteLine("o numero que você digitou não pode ser 0");
+                        }
+                        else
+                        {
+                            p.total = p.num1 * p.num2;
+                            Console.WriteLine("o total é " + p.total);
+                        }
+                        break;
+
+
+                }
+                Console.WriteLine("digite 1 para continuar ou digite 0 para sair");
+                p.aux = int.Parse(Console.ReadLine());
+            }
+            Console.ReadKey();
+            Console.WriteLine("tchau");
         }
     }
 }
---
+++ calculadora/numeros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    class numeros
+    {
+        public float num1;
+        public float num2;
+        public float total;
+        public int cal;
+        public int aux=1;
+    }
+}
